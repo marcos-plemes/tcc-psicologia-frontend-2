6465b59426932f1e5681239f8e9fa9005f490d08
Marcos Vinicius <mvplemes@gmail.com>
2024-09-29 21:14:53 -0300

    testes

---
+++ src/app/componentes/comum/base.service.ts
@@ -0,0 +1,37 @@
+import { HttpParams } from "@angular/common/http";
+import { environment } from "../../../environment/environment";
+
+export abstract class BaseService {
+  constructor() {
+  }
+
+  getUrbase(): string {
+    return `${environment.protocoloHttp}://${environment.baseUrl}`;
+  }
+
+  montarParametros(obj: Object) {
+    return { params: this.toHttpParams(obj, new HttpParams()) };
+  }
+
+  private toHttpParams(obj: Object, params: HttpParams): HttpParams {
+
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+
+        const val = obj[key as keyof Object];
+        if (val !== null && val !== undefined) {
+
+          if (typeof val === 'object') {
+            params = this.toHttpParams(val, params);
+
+          } else {
+            params = params.append(key, val.toString());
+          }
+        }
+      }
+    }
+
+    return params;
+  }
+
+}
---
+++ src/app/environment/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  protocoloHttp: 'https',
+  baseUrl: 'tcc-psicologia-backend.onrender.com',
+  api: ''
+  // baseUrl: `${window.location.origin.split('//')[1]}`
+};
