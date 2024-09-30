2654144817cf704cf5929e55d54ec46487e83d57
Marcos Vinicius <mvplemes@gmail.com>
2024-09-29 21:57:51 -0300

    testse

--- src/app/componentes/perguntas/perguntas/perguntas.component.ts
+++ src/app/componentes/perguntas/perguntas/perguntas.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PerguntasService } from "../perguntas.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-perguntas',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './perguntas.component.scss'
 })
 export class PerguntasComponent {
+  loading: boolean = true;
+
+  codigo: number | null = null;
+
+  perguntasService: PerguntasService;
+
+  form: FormGroup;
+
+  selectedFile: File | null = null;
+
+  imageUrl: string | ArrayBuffer | null | undefined = null;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    fromBuilder: FormBuilder,
+    perguntasService: PerguntasService) {
+
+    this.perguntasService = perguntasService;
+    this.form = fromBuilder.group({
+      codigo: [],
+      descricao: ['', [Validators.required, Validators.minLength(10)]],
+      imagem: ['', [Validators.required]]
+    });
+  }
 
 }
