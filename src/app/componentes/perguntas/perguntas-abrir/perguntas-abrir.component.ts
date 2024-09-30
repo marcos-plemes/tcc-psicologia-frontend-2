c5159227495ce9b47f898f8c30a63c7905c59456
Marcos Vinicius <mvplemes@gmail.com>
2024-09-29 21:39:48 -0300

    ttttt

--- src/app/componentes/perguntas/perguntas-abrir/perguntas-abrir.component.ts
+++ src/app/componentes/perguntas/perguntas-abrir/perguntas-abrir.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pergunta } from "../pergunta.interface";
+import { PerguntasService } from "../perguntas.service";
 
 @Component({
   selector: 'app-perguntas-abrir',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './perguntas-abrir.component.scss'
 })
 export class PerguntasAbrirComponent {
+  perguntas: Pergunta[] = [];
+
+  perguntasService: PerguntasService;
+
+  constructor(perguntasService: PerguntasService) {
+    this.perguntasService = perguntasService;
+  }
+
+  deletarPergunta(pergunta: Pergunta) {
+    if (pergunta.codigo) {
+      this.perguntasService.deletarPergunta(pergunta.codigo).subscribe(response => {
+        alert('Pergunta deletada com sucesso');
+        this.perguntas = this.perguntas.filter(p => p.codigo !== pergunta.codigo);
+      }, error => {
+        alert('Erro ao deletar pergunta');
+      });
+    }
+  }
+
+
 
 }
