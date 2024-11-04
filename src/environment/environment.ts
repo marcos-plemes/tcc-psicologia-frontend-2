c154501fa644a94219c195828a6aefa08246d075
Marcos Vinicius <mvplemes@gmail.com>
2024-11-03 21:14:45 -0300

    teste

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: false,
-  protocoloHttp: 'http',
-  baseUrl: 'localhost:5000',
-  // protocoloHttp: 'https',
-  // baseUrl: 'tcc-psicologia-backend.onrender.com',
+  // protocoloHttp: 'http',
+  // baseUrl: 'localhost:5000',
+  protocoloHttp: 'https',
+  baseUrl: 'tcc-psicologia-backend.onrender.com',
   api: ''
-  // baseUrl: `${window.location.origin.split('//')[1]}`
 };
